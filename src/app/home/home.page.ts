0678c672cca8fd517faefc37d6d7f4c1a77c2380
Wemerson Vital <wemerson.thayne@gmail.com>
2019-06-30 09:49:15 -0300

    Create Project to open camera with PWA

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource, CameraDirection  } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,71 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // imageURL: any;
+  captures: Array<any>;
+  video: any;
+  constraints: any = {
+    video: true
+  };
+
+  constructor() {
+    this.captures = [];
+  }
+
+  ngOnInit() {
+    
+  }
+
+  async takePhoto(){
+    const { Camera } = Plugins;
+      Camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        correctOrientation: true,
+        direction: CameraDirection.Rear,
+        source:CameraSource.Camera,
+        saveToGallery:false
+      }).then(image => {
+        let imageUrl = image.webPath;
+        console.log(imageUrl);
+        // this.imageURL.src = imageUrl;
+      })
+  }
+
+  public ngAfterViewInit() {
+
+    this.video = document.getElementById('video');
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      /*setting capture only video*/
+      navigator.mediaDevices.getUserMedia(this.constraints).then((stream) => {
+        /*set stream on element*/
+        this.video.srcObject = stream;
+        this.video.play();
+        //this.video = video;
+      }, (error) => {
+        console.log('Error to getStream camera: ', error);
+      });
+    }
+
+  }
+
+
+  public capture() {
+    let canvas: any = document.getElementById('canvas');
+    let context: any = canvas.getContext('2d');
+    // Trigger photo take
+    context.drawImage(this.video, 0, 0, canvas.width, canvas.height);
+    this.captures.push(canvas.toDataURL("image/png"));
+    console.log(this.captures);
+    // this.stopCamera();
+  }
+
+  private stopCamera() {
+    this.video.srcObject.getVideoTracks().forEach(track => {
+      track.stop();
+    });
+  }
 
 }
